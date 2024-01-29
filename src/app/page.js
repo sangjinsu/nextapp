c3337e775e55b5bda6f53477b292362ffc84524e
sangjinsu <climbplant39@kakao.com>
2024-01-29 21:44:18 +0900

    ✨ layout and page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-    <h1>Hello Next.js</h1>
-    </div>
-  );
+    return (
+        <>
+            <h1>Hello Next.js</h1>
+            Hello, Web
+        </>
+    );
 }
