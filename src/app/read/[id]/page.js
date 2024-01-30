2174adbdcd0d3151eaaf5e363a1dfd0f8b856758
sangjinsu <climbplant39@kakao.com>
2024-01-30 20:03:12 +0900

    ✨ add update feature

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,5 +1,5 @@
 export default async function Read({params: {id}}) {
-    const resp = await fetch(`http://localhost:9999/topics/${id}`);
+    const resp = await fetch(`http://localhost:9999/topics/${id}`, {cache: 'no-cache'});
     const topic = await resp.json();
     return <>
         <h2>{topic.title}</h2>
