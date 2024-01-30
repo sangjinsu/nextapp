f7d4bb06048cf055767375084596cf171ecd6221
sangjinsu <climbplant39@kakao.com>
2024-01-30 20:05:29 +0900

    ✨ add delete feature

--- src/app/Control.js
+++ src/app/Control.js
@@ -1,15 +1,27 @@
 "use client"
 import Link from 'next/link';
-import {useParams} from 'next/navigation';
+import {useParams, useRouter} from 'next/navigation';
 
 export function Control() {
     const params = useParams();
+    const router = useRouter();
+
     const id = params.id;
+
+    const onDelete = async () => {
+        const resp = await fetch(`http://localhost:9999/topics/${id}`, {
+            method: 'DELETE'
+        });
+        await resp.json();
+        router.push('/');
+        router.refresh();
+    }
+
     return (
         <ul>
             {id ? <>
                 <li><Link href={"/update/" + id}>Update</Link></li>
-                <li><input type="button" value="delete"/></li>
+                <li><input onClick={onDelete} type="button" value="delete"/></li>
             </> : <li><Link href="/create">Create</Link></li>}
         </ul>
     );
